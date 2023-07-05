58a2c75e5e7235b072ae6a21616552cb5986b877
Christian Sidebang <117734590+csidebang02@users.noreply.github.com>
2023-07-05 23:37:36 +0700

    mengubah

--- Day 3/Tugas Foo Bar/Program.cs
+++ Day 3/Tugas Foo Bar/Program.cs
@@ -7,11 +7,11 @@
 
         for (int i = 0; i < n; i++)
         {
-            if (i % 3 == 0 && i % 5 == 0)
+            if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0)
                 Console.Write("foobar ");
-            else if (i + 1 % 3 == 0)
+            else if ((i + 1) % 3 == 0)
                 Console.Write("foo ");
-            else if (i + 1 % 5 == 0)
+            else if ((i + 1) % 5 == 0)
                 Console.Write("bar ");
             else
                 Console.Write($"{i + 1} ");
