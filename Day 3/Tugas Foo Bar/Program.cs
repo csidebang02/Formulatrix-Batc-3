dc20a3de8fffd6c998c4e97ed5e77ee7e3669c00
Christian Sidebang <117734590+csidebang02@users.noreply.github.com>
2023-07-06 20:23:13 +0700

    ubah Foobar

--- Day 3/Tugas Foo Bar/Program.cs
+++ Day 3/Tugas Foo Bar/Program.cs
@@ -1,20 +1,24 @@
 ﻿class Program
 {
-    static void Main(string[] args)
-    {
-        Console.Write("Masukkan angka: ");
-        int n = Convert.ToInt32(Console.ReadLine());
+	static void Main(string[] args)
+	{
+		Console.Write("Masukkan angka: ");
+		int n = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine ("n =" + n);
 
-        for (int i = 0; i < n; i++)
-        {
-            if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0)
-                Console.Write("foobar ");
-            else if ((i + 1) % 3 == 0)
-                Console.Write("foo ");
-            else if ((i + 1) % 5 == 0)
-                Console.Write("bar ");
-            else
-                Console.Write($"{i + 1} ");
-        }
-    }
+		for (int i = 0; i <= n; i++)
+		{
+			if (i == 0)
+				Console.WriteLine(i);
+			else if(i % 3 == 0 && i % 5 == 0)
+				Console.WriteLine("foobar ");
+			else if (i % 3 == 0)
+				Console.WriteLine("foo ");
+			else if (i % 5 == 0)
+				Console.WriteLine("bar ");
+			else
+				Console.WriteLine(i);
+		}
+		Console.ReadLine();
+	}
 }
