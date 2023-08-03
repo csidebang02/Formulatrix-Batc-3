706c9e53110f99b5c2f6a0c81f12d26050a1260a
Adibya Rizaldy <117734590+csidebang02@users.noreply.github.com>
2023-08-03 12:04:14 +0700

    Day 20

---
+++ Day 20/Directory/Program.cs
@@ -0,0 +1,22 @@
+﻿class program
+{
+	static void Main()
+	{
+		string path = @"C:\Users\Batch 3\Desktop\Formulatrix-Batc-3\Formulatrix-Batc-3\Formulatrix-Batc-3\Formulatrix-Batc-3-1\Day 20\AsyncFile";
+		Directory.CreateDirectory(path);
+		
+		if (Directory.Exists(path))
+		{
+			Console.WriteLine("Directory");
+		}
+		string filePath  = Path.Combine(path, "example.txt");
+		File.WriteAllText(filePath, "Hello, AsyncFile!");
+		
+		string[] files = Directory.GetFiles(path);
+		foreach (string file in files)
+		{
+			Console.WriteLine(file);
+		}
+		Directory.Delete(path, true);
+	}
+}
