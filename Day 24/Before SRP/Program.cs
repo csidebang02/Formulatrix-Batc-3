f30f728bcdc14639823f649821b7e45c1d53cdad
Adibya Rizaldy <117734590+csidebang02@users.noreply.github.com>
2023-08-10 11:10:45 +0700

    Before SRP

---
+++ Day 24/Before SRP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		string merk = "Kawasaki";
+		string tipe = "Ninja";
+		int tahunProduksi = 2015;
+		string pemilik = "Christian Sidebang";
+		string alamatPemilik = "Semarang";
+		
+		Console.WriteLine($"Melayani pemeriksaan mesin motor {merk} {tipe}...");
+		Console.WriteLine("Pemeriksaan mesin selesai. Tidak ditemukan masalah.");
+		
+		Console.WriteLine($"Melayani penggantian oli motor {merk} {tipe}...");
+		Console.WriteLine("Penggantian oli selesai.");
+		
+		Console.WriteLine($"Melayani penggantian ban motor {merk} {tipe}...");
+		Console.WriteLine("Penggantian ban selesai.");
+		
+		Console.WriteLine ($"Motor milik {pemilik} ({alamatPemilik}) telah diperbaiki.");
+		
+		Console.WriteLine("Program Selesai");
+		
+	}
+}
